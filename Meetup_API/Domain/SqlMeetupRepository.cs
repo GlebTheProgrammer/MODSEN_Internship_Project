8a7c9864f1ff83b07dc582454cf6e2b9269e0493
Gleb Sukristik <gleb15a@gmail.com>
2022-07-27 22:28:01 +0300

    Implemented new methods in the SqlMeetupRepository

--- Meetup_API/Domain/SqlMeetupRepository.cs
+++ Meetup_API/Domain/SqlMeetupRepository.cs
@@ -11,6 +11,16 @@ namespace Meetup_API.Domain
             this.context = context;
         }
 
+        public void CreateEvent(Event eventItem)
+        {
+            if (eventItem == null)
+                throw new ArgumentNullException(nameof(eventItem));
+
+            context.Events.Add(eventItem);
+            SaveChanges();
+            return;
+        }
+
         public Event GetEventById(int id)
         {
             return context.Events.FirstOrDefault(eventItem => eventItem.Id == id);
@@ -19,6 +29,11 @@ namespace Meetup_API.Domain
         public IEnumerable<Event> GetEvents()
         {
             return context.Events.ToList();
+        }
+
+        public bool SaveChanges()
+        {
+            return context.SaveChanges() >= 0;
         }
     }
 }
