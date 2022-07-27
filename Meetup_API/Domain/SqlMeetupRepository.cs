c5d45a70d5fc64f2024a5f3e38e637ce5f72449e
Gleb Sukristik <gleb15a@gmail.com>
2022-07-27 21:50:42 +0300

    Added sql repository implementation to manipulate with DB

---
+++ Meetup_API/Domain/SqlMeetupRepository.cs
@@ -0,0 +1,24 @@
+﻿using Meetup_API.Models;
+
+namespace Meetup_API.Domain
+{
+    public class SqlMeetupRepository : IMeetupRepository
+    {
+        private readonly MeetupDbContext context;
+
+        public SqlMeetupRepository(MeetupDbContext context)
+        {
+            this.context = context;
+        }
+
+        public Event GetEventById(int id)
+        {
+            return context.Events.FirstOrDefault(eventItem => eventItem.Id == id);
+        }
+
+        public IEnumerable<Event> GetEvents()
+        {
+            return context.Events.ToList();
+        }
+    }
+}
