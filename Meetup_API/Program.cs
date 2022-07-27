7e44bbb977bad92a8bea0353198dc8cfdf57520e
Gleb Sukristik <gleb15a@gmail.com>
2022-07-27 21:58:36 +0300

    Added AutoMapper NuGet package

--- Meetup_API/Program.cs
+++ Meetup_API/Program.cs
@@ -16,6 +16,8 @@ builder.Services.AddScoped<IMeetupRepository, SqlMeetupRepository>();
 builder.Services.AddDbContext<MeetupDbContext>(options => options.UseSqlServer
 (builder.Configuration.GetConnectionString("MeetupConnection")));
 
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
