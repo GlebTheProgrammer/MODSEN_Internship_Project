790ff6a208a5f7d9f40b8eb6db981bb668b1157f
Gleb Sukristik <gleb15a@gmail.com>
2022-07-27 22:02:47 +0300

    Added DTO Model (Id field is not included in it)

---
+++ Meetup_API/DTOs/EventReadDto.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Meetup_API.DTOs
+{
+    public class EventReadDto
+    {
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public string Plan { get; set; }
+
+        public string Organizer { get; set; }
+
+        public string Speaker { get; set; }
+
+        public string Date { get; set; }
+
+        public string Place { get; set; }
+    }
+}
