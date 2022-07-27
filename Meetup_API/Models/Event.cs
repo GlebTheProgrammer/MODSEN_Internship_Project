e0518f1d5a94fef5dd8907c0a63b5247ed5af5e6
Gleb Sukristik <gleb15a@gmail.com>
2022-07-27 20:52:44 +0300

    Added data annotations to our model

--- Meetup_API/Models/Event.cs
+++ Meetup_API/Models/Event.cs
@@ -1,14 +1,36 @@
-﻿namespace Meetup_API.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Meetup_API.Models
 {
     public class Event
     {
+        [Key]
         public int Id { get; set; }
+
+        [Required]
+        [MaxLength(255)]
         public string Name { get; set; }
+
+        [Required]
         public string Description { get; set; }
+
+        [Required]
         public string Plan { get; set; }
+
+        [Required]
+        [MaxLength(255)]
         public string Organizer { get; set; }
+
+        [Required]
+        [MaxLength(255)]
         public string Speaker { get; set; }
+
+        [Required]
+        [MaxLength(12)]
         public string Date { get; set; }
+
+        [MaxLength(255)]
+        [Required]
         public string Place { get; set; }
     }
 }
