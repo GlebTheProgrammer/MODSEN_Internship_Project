598c3af776349068afc19c50da643e7f9abbfa2d
Gleb Sukristik <gleb15a@gmail.com>
2022-07-27 20:36:55 +0300

    Added class wich will be used to create our DB

---
+++ Meetup_API/Domain/MeetupDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Meetup_API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Meetup_API.Domain
+{
+    public class MeetupDbContext : DbContext
+    {
+        public MeetupDbContext(DbContextOptions<MeetupDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Event> Events { get; set; }
+    }
+}
