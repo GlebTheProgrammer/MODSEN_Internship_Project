010f3e7ae56f49596050a7ebd546d3932901651b
Gleb Sukristik <gleb15a@gmail.com>
2022-07-27 19:26:40 +0300

    Added Event model

---
+++ Meetup_API/Models/Event.cs
@@ -0,0 +1,13 @@
+﻿namespace Meetup_API.Models
+{
+    public class Event
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Plan { get; set; }
+        public string Organizer { get; set; }
+        public string Speaker { get; set; }
+        public string Date { get; set; }
+        public string Place { get; set; }
+    }
+}
