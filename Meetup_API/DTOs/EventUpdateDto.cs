0913fba2b25f4d7f85256baed5dcd17a091c12fb
Gleb Sukristik <gleb15a@gmail.com>
2022-07-28 09:21:11 +0300

    Added updateEvent dto

---
+++ Meetup_API/DTOs/EventUpdateDto.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Meetup_API.DTOs
+{
+    public class EventUpdateDto
+    {
+        [Required]
+        [MaxLength(255)]
+        public string Name { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+        [Required]
+        public string Plan { get; set; }
+
+        [Required]
+        [MaxLength(255)]
+        public string Organizer { get; set; }
+
+        [Required]
+        [MaxLength(255)]
+        public string Speaker { get; set; }
+
+        [Required]
+        [MaxLength(12)]
+        public string Date { get; set; }
+
+        [MaxLength(255)]
+        [Required]
+        public string Place { get; set; }
+    }
+}
