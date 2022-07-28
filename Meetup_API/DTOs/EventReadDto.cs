298f10a9796abf79f405c92e9839aa79c22ea492
Gleb Sukristik <gleb15a@gmail.com>
2022-07-28 09:39:57 +0300

    Added Id field to EventReadDto

--- Meetup_API/DTOs/EventReadDto.cs
+++ Meetup_API/DTOs/EventReadDto.cs
@@ -4,6 +4,8 @@ namespace Meetup_API.DTOs
 {
     public class EventReadDto
     {
+        public int Id { get; set; }
+
         public string Name { get; set; }
 
         public string Description { get; set; }
